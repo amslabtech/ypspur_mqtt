d91910f414103c5c7816c33a1ee7a00ea33dd6c1
taka-kazu <tkhsh.schoen@gmail.com>
2019-12-14 00:14:08 +0900

    WIP: adding mqtt pub/sub

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
 #include "ypspur_wrapper.h"
 #include "mosquitto.hpp"
+#include "time.h"
+#include "sys/time.h"
+
+class CommandVelocity : public Mosquitto {
+  protected:
+    const char* topic;
+    void onConnected();
+    void onError(const char* _msg) { std::cout << _msg; }
+    void onMessage(std::string _topic, void* _data, int _len);
+    struct timeval ts;
+
+  public:
+    CommandVelocity(const char* _topic):topic(_topic){}
+
+    YPSpurWrapper::Velocity cmd_vel;
+};
+
+void CommandVelocity::onConnected()
+{
+    std::cout << "Connected to MQTT broker." << std::endl;;
+    subscribe(topic);
+}
+
+void CommandVelocity::onMessage(std::string _topic, void* _data, int _len)
+{
+    gettimeofday(&ts, NULL);
+    std::cout << (char*)_data << std::endl;
+    // bcopy(_data, (char*)&cmd_vel, sizeof(cmd_vel));
+    // std::cout << cmd_vel << std::endl;
+}
 
 int main(int argc, char** argv)
 {
@@ -21,20 +51,28 @@ int main(int argc, char** argv)
         }
     }
 
-    Mosquitto mqtt;
-    const char* ip_addr  = "localhost";
-    const char* cmd_vel_topic    = "cmd_vel";
-    mqtt.connect(ip_addr);
+    const char* ip_addr = "localhost";
+    const char* cmd_vel_topic = "cmd_vel";
+    const char* odom_topic = "odom";
 
+    Mosquitto odom_publisher;
+    odom_publisher.connect(ip_addr);
+
+    CommandVelocity cmd_vel_listener(cmd_vel_topic);
+    cmd_vel_listener.connect(ip_addr);
+    cmd_vel_listener.loop_start();
     try{
         ypspur_wrapper->initialize();
         while(!ypspur_wrapper->is_shutdown_requested() && ypspur_wrapper->spin_once()){
             std::cout << "main loop" << std::endl;
+            YPSpurWrapper::Odometry odom = ypspur_wrapper->get_odometry();
+            // odom_publisher.publish(odom_topic, (void*)&odom, sizeof(odom));
             usleep(1e6);
         }
     }catch(std::exception& e){
         std::cerr << e.what() << std::endl;
     }
     delete ypspur_wrapper;
+    cmd_vel_listener.cleanup_library();
     return 0;
 }
